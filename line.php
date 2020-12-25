46fe2ef6fb6a505628eba9c5cc7ed36ef3af4093
champcm <13belove@gmail.com>
2020-12-25 13:54:53 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'Uae59140384d613ef352fcb2cab626f08',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
